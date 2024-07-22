4cb12f55f7b927db15ef09ed9fe8c098e6d8c564
Dan Courtness <danielcourtness@gmail.com>
2024-07-22 23:23:29 +0900

    fix: attempt cache refresh for twitter share image

--- theme.config.tsx
+++ theme.config.tsx
@@ -82,7 +82,7 @@ const config: DocsThemeConfig = {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Abstract" />
       <meta name="twitter:description" content="Abstract Documentation" />
-      <meta name="twitter:image" content="https://docs.abs.xyz/share.jpg" />
+      <meta name="twitter:image" content="https://docs.abs.xyz/share.jpg?v=0" />
       <meta name="twitter:site" content="@AbstractChain" />
       <meta name="twitter:creator" content="@AbstractChain" />
 
